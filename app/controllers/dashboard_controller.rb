0f9b83132246301583c1521cb01aa9a8283b41b3
Brendan Hanks <ex.actionmodel@gmail.com>
2014-05-30 11:02:14 -0500

    started work on subclasses

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,12 @@
 class DashboardController < ApplicationController
   def products
-    @products = Product.all
+    type = ProductsController::CONTROLLER_TO_MODEL[params[:action]]
+    @products = Product.where(type: type)
+    render "products"
   end
+
+  def games
+    products
+  end
+
 end
---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,3 @@
+class GamesController < ProductsController
+
+end
