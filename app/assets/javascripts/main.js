40ca61d55a310251c33be1a8acf0de40266a82af
Brendan Hanks <ex.actionmodel@gmail.com>
2014-07-29 09:41:28 -0500

    added area for features on front page

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
   $('.title').each(function(){
-
+    $(this).click(function(){
+      if($('.split').is(":visible") == false)
+        $(".split").slideDown();
+      var target = $(this).data("arrow");
+      $('.arrow').animate({
+          left: target
+        }
+        ,500
+        ,function(){})
+    });
   });
 
 });
