43950cf1554b55c64beb2ef35999ad029f660691
Brendan Hanks <ex.actionmodel@gmail.com>
2014-08-01 16:42:49 -0500

    finished scrolly text thing

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,14 +1,16 @@
 $(document).ready(function(){
-  $('.title').each(function(){
+  $('.featured').each(function(){
     $(this).click(function(){
       if($('.split').is(":visible") == false)
         $(".split").slideDown();
       var target = $(this).data("arrow");
+      var shift = $(this).data("shift");
       $('.arrow').animate({
           left: target
         }
         ,500
-        ,function(){})
+        ,function(){});
+      $('.details').animate({bottom:shift},500);
     });
   });
   
