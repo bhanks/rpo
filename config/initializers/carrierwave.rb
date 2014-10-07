519c900833f0a3c64093d9dcf8687b60fa41b054
Brendan Hanks <ex.actionmodel@gmail.com>
2014-10-07 09:53:49 -0500

    heroku aws stuff

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,6 +6,7 @@ CarrierWave.configure do |config|
     :aws_access_key_id      => ENV['S3_KEY'],
     :aws_secret_access_key  => ENV['S3_SECRET'],                        # required
   }
+   
   config.fog_directory  = 'forbiddenplanet-assets'                          # required
   config.fog_public     = false                                        # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>"max-age=#{365.day.to_i}"} # optional, defaults to {}
